74f63cf62e54b91187fab49f6189afd097568b4f
IVYMAX <ivyli@IVYMAXmbp.lan>
2019-08-17 04:28:38 +1000

    Updates

---
+++ precache-manifest.28d5f1f823be4c2b813675e612d173c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2a017c5cd121eaebdab7846615d78c5",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "35bcef4318a4cf965d54",
+    "url": "/weatherapp/static/css/main.0be26e96.chunk.css"
+  },
+  {
+    "revision": "5b210c0bb0e3ff86cdc0",
+    "url": "/weatherapp/static/js/2.ac366411.chunk.js"
+  },
+  {
+    "revision": "35bcef4318a4cf965d54",
+    "url": "/weatherapp/static/js/main.b1636f49.chunk.js"
+  },
+  {
+    "revision": "5bfbd7a83723d1d6555c",
+    "url": "/weatherapp/static/js/runtime~main.e7dc3139.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.f18973c63240d120e24f7df3a37d0947.js"
+  "/weatherapp/precache-manifest.28d5f1f823be4c2b813675e612d173c9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/weatherapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
